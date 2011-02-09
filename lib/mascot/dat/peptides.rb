5bdbbd4196439d5ceba0d81168c1766bb8637d10
Angel Pizarro <angel@upenn.edu>
2011-02-09 11:46:17 -0500

    moved psm parsing out of peptides to psm class

--- lib/mascot/dat/peptides.rb
+++ lib/mascot/dat/peptides.rb
@@ -95,53 +95,10 @@ module Mascot
         break unless l =~ /^q#{q}_p#{p}/
         tmp << l.chomp
       end
-      parse_psm(tmp)
+      Mascot::DAT::PSM.parse(tmp)
     end
 
-    def parse_psm psm_arr
-      psm_result = ::Mascot::DAT::PSM.new()
 
-      psm_arr.each do |l|
-        k,v = l.split "="
-        case k
-        when /^q(\d+)_p(\d+)$/
-          #main result, must split value
-          psm_vals, prots  = v.split(";")
-          psm_vals = psm_vals.split(',')
-          psm_result.query = $1.to_i
-          psm_result.rank = $2.to_i
-          psm_result.missed_cleavages= psm_vals[0].to_i
-          psm_result.mr              = psm_vals[1].to_f
-          psm_result.delta           = psm_vals[2].to_f
-          psm_result.num_ions_matched = psm_vals[3].to_i
-          psm_result.pep             = psm_vals[4]
-          psm_result.ions1           = psm_vals[5].to_i
-          psm_result.var_mods_str    = psm_vals[6]
-          psm_result.score           = psm_vals[7].to_f
-          psm_result.ion_series_str  = psm_vals[8]
-          psm_result.ions2           = psm_vals[9].to_i
-          psm_result.ions3           = psm_vals[10].to_i
-
-          # assign proteins
-          psm_result.proteins = prots.split(",").map do |pe|
-            acc,*other_vals =  pe.split(":")
-            acc.gsub!(/\"/,'')
-            [acc] + other_vals.map {|e| e.to_i }
-          end
-        when /db$/
-          # split on 2 chars, call to_i
-          psm_result.dbs = v.split(/(\d{2})/).grep(/^\d+$/) { |e| e.to_i }
-        when /terms$/
-          # for each protein, I have to add the term AA
-          psm_result.terms = v.split(":").collect {|t| t.split(",") }
-        else
-          # returns the smaller key
-          k_sym = k.slice(/q\d+_p\d+_?(.+)/,1).to_sym
-          psm_result.attrs[k_sym] = v
-        end
-      end
-      psm_result
-    end
 
     def each
       while @file.pos < @endbytepos
--- lib/mascot/dat/psm.rb
+++ lib/mascot/dat/psm.rb
@@ -1,6 +1,7 @@
 module Mascot
   class DAT
     class PSM
+
       ATTRS = [:query,:rank,:missed_cleavages,:mr, :delta,
         :num_ions_matched,:pep,:ions1,:var_mods_str,:score,
         :ion_series_str,:ions2,:ions3,:proteins,:dbs,:terms,:attrs]
@@ -30,6 +31,50 @@ module Mascot
           end
         end
         is_eql
+      end
+      def self.parse psm_arr
+        psm_result =  self.new()
+
+        psm_arr.each do |l|
+          k,v = l.split "="
+          case k
+          when /^q(\d+)_p(\d+)$/
+            #main result, must split value
+            psm_vals, prots  = v.split(";")
+            psm_vals = psm_vals.split(',')
+            psm_result.query = $1.to_i
+            psm_result.rank = $2.to_i
+            psm_result.missed_cleavages= psm_vals[0].to_i
+            psm_result.mr              = psm_vals[1].to_f
+            psm_result.delta           = psm_vals[2].to_f
+            psm_result.num_ions_matched = psm_vals[3].to_i
+            psm_result.pep             = psm_vals[4]
+            psm_result.ions1           = psm_vals[5].to_i
+            psm_result.var_mods_str    = psm_vals[6]
+            psm_result.score           = psm_vals[7].to_f
+            psm_result.ion_series_str  = psm_vals[8]
+            psm_result.ions2           = psm_vals[9].to_i
+            psm_result.ions3           = psm_vals[10].to_i
+
+            # assign proteins
+            psm_result.proteins = prots.split(",").map do |pe|
+              acc,*other_vals =  pe.split(":")
+              acc.gsub!(/\"/,'')
+              [acc] + other_vals.map {|e| e.to_i }
+            end
+          when /db$/
+            # split on 2 chars, call to_i
+            psm_result.dbs = v.split(/(\d{2})/).grep(/^\d+$/) { |e| e.to_i }
+          when /terms$/
+            # for each protein, I have to add the term AA
+            psm_result.terms = v.split(":").collect {|t| t.split(",") }
+          else
+            # returns the smaller key
+            k_sym = k.slice(/q\d+_p\d+_?(.+)/,1).to_sym
+            psm_result.attrs[k_sym] = v
+          end
+        end
+        psm_result
       end
     end
   end
